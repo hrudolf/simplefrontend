09bfa5b822282a13e1ad72e9b0fae91bd8114c75
hrudolf <h.rudolf@gmail.com>
2023-05-30 12:38:52 +0200

    Add change to main site

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload.<br></br>
+          This is a new commit, container should be create automatically!
         </p>
         <a
           className="App-link"
